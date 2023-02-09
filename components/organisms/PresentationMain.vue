08be9c305602da586c5fbeedf7b62096034ecebc
Bruna Scauri Splettstoser <brunascauri@hotmail.com>
2023-02-09 19:52:13 -0300

    feat: presentation section

---
+++ components/organisms/PresentationMain.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="presentation">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ab tenetur laborum fugiat perspiciatis. Eligendi, inventore labore! Quas repellat optio nisi in laudantium, minus voluptatem assumenda ullam dolorem atque aspernatur!Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ab tenetur laborum fugiat perspiciatis. Eligendi, inventore labore! Quas repellat optio nisi in laudantium, minus voluptatem assumenda ullam dolorem atque aspernatur!Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ab tenetur</p>
+
+    <img src='@/assets/img/presentation.svg' alt="apresentação do ibook">
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.presentation {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 2rem;
+  align-items: center;
+  @include screen('small', 'medium') {
+    grid-template-columns: 1fr;
+  }
+  p {
+    line-height: 26px;
+  }
+  img {
+    max-width: 100%;
+  }
+}
+</style>
