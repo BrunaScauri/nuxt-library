a11b49379d1032c77ce4f16ca210c77c593b37ba
Bruna Scauri Splettstoser <brunascauri@hotmail.com>
2023-02-09 11:29:37 -0300

    feat: home and about pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <Tutorial />
+  <div>
+    <ul>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/sobre">Sobre</NuxtLink>
+
+    </ul>
+
+
+    <h1>Sobre</h1>
+  </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
-  name: 'IndexPage',
-})
-</script>
---
+++ pages/sobre.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <ul>
+      <NuxtLink to="/home">Home</NuxtLink>
+      <NuxtLink to="/">Sobre</NuxtLink>
+    </ul>
+
+
+    <h1>Home</h1>
+  </div>
+</template>
